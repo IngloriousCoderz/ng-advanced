79f5470088ea1d72f79899cc4de1b81a048f1273
IceOnFire <antony.mistretta@gmail.com>
2017-11-25 11:49:42 +0100

    Create initial app with FormsModule and working test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,52 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+import { TestBed, async } from "@angular/core/testing";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+
+describe("AppComponent", () => {
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [FormsModule],
+        declarations: [AppComponent]
+      }).compileComponents();
+    })
+  );
+
+  it(
+    "should create the app",
+    async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    })
+  );
+
+  it(
+    `should have an empty text`,
+    async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.text).toEqual("");
+    })
+  );
+
+  it(
+    `should have a list of todos`,
+    async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.todos.length).toBe(3);
+    })
+  );
+
+  it(
+    "should render the todo text in each <li> tag",
+    async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector("li").textContent).toContain("Todo");
+    })
+  );
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  text = "";
+
+  todos = [
+    { id: 1, text: "Todo 1", done: true },
+    { id: 2, text: "Todo 2", done: false },
+    { id: 3, text: "Todo 3" }
+  ];
+
+  onSubmit() {
+    const id = this.todos.length ? this.todos[this.todos.length - 1].id : 0;
+    this.todos.push({ id: id + 1, text: this.text });
+    this.text = "";
+  }
+
+  toggleDone(todo) {
+    todo.done = !todo.done;
+  }
 }
